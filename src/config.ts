fbc40e70e9cfb99f5ef87a55a8ca8eac83550bdd
Alexey Poimtsev <alexey.poimtsev@gmail.com>
2023-09-19 01:03:46 +0300

    fix: fixed layout

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   title: "I'm Mary Poppins, y'all!",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 10,
+  postPerPage: 5,
 };
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
