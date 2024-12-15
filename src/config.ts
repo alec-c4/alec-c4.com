3ca68e1a9697be3101d758bdc681d276161465c4
Alexey Poimtsev <alexey.poimtsev@gmail.com>
2024-12-15 20:09:10 +0300

    content: migrate from importmap to esbuild

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   title: "I'm Mary Poppins, y'all!",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 5,
+  postPerPage: 10,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
