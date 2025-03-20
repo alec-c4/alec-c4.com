6708912a2b3379ea888d2e91e14e45fcd4e4b4d3
Alexey Poimtsev <alexey.poimtsev@gmail.com>
2025-03-20 12:40:19 +0300

    chore: update to 5e92a3d

--- src/config.ts
+++ src/config.ts
@@ -12,6 +12,7 @@ export const SITE = {
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
+    enabled: true,
     url: "https://github.com/alec-c4/alec-c4.com/edit/master/src/data/blog",
     text: "Suggest Changes",
     appendFilePath: true,
