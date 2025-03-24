d44d57e4d54c1e4d3f8673530499b7feb2baafbe
Alexey Poimtsev <alexey.poimtsev@gmail.com>
2025-03-24 22:28:25 +0300

    chore: update astro-paper to 5.2.0 (de8da9e)

--- src/config.ts
+++ src/config.ts
@@ -18,4 +18,6 @@ export const SITE = {
     appendFilePath: true,
   },
   dynamicOgImage: true,
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  timezone: "Europe/Moscow", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
